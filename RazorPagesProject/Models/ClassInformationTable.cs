4d030932bf9bbd3f31998ae7b9f78e948e0d5044
elif2021 <elifgngr02@gmail.com>
2025-04-30 14:20:58 +0300

    week7 added

--- RazorPagesProject/Models/ClassInformationTable.cs
+++ RazorPagesProject/Models/ClassInformationTable.cs
@@ -1,15 +1,10 @@
-using System.ComponentModel.DataAnnotations;
-
-public class ClassInformationTable
+namespace RazorPagesProject.Models
 {
-    // ID tabloda gösterilmeyecek ancak arka planda kullanılacak.
-    public int Id { get; set; }
-
-    [Required(ErrorMessage = "Class Name is required.")]
-    public string ClassName { get; set; } = string.Empty;
-
-    public int StudentCount { get; set; }
-
-    [Required(ErrorMessage = "Description is required.")]
-    public string Description { get; set; } = string.Empty;
+    public class ClassInformationTable
+    {
+        public int Id { get; set; }
+        public string ClassName { get; set; } = string.Empty;
+        public int StudentCount { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
 }
