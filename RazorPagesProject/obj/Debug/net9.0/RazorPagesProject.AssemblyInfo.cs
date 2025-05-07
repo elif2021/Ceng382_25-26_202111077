531651943f09e5ffe7ff9fc646322ae2fb910381
elif2021 <elifgngr02@gmail.com>
2025-05-07 14:57:14 +0300

    week8

--- RazorPagesProject/obj/Debug/net9.0/RazorPagesProject.AssemblyInfo.cs
+++ RazorPagesProject/obj/Debug/net9.0/RazorPagesProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcbcc2bd4aec66e6cdc3847f4c125b43d2c3597b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05b3e01b490f1eb253af59a7d0f2856d8580aeae")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
