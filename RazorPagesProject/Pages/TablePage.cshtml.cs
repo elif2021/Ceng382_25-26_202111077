c57c9153dc1e6dff301f4b4609d90731208f5aff
elif2021 <elifgngr02@gmail.com>
2025-05-09 12:59:17 +0300

    added week8...

--- RazorPagesProject/Pages/TablePage.cshtml.cs
+++ RazorPagesProject/Pages/TablePage.cshtml.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace RazorPagesProject.Pages
 {
     public class TablePageModel : PageModel
-    {
+    {}
        public string? Username { get; set; }
 
     public void OnGet()
